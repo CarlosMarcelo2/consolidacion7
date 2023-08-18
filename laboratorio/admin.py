1fda3b534992267134898dc3441676ee4c9202f2
Carlos Samamé <cmsamamer@gmail.com>
2023-08-18 02:02:05 +0000

    tercera migracion

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -3,6 +3,33 @@ from django.contrib import admin
 # Register your models here.
 from .models import Laboratorio, DirectorGeneral, Producto
 
-admin.site.register (Laboratorio)
-admin.site.register (DirectorGeneral)
-admin.site.register (Producto)
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    
+admin.site.register (Laboratorio, LaboratorioAdmin)
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio')
+    ordering = ('nombre', )
+
+admin.site.register (DirectorGeneral, DirectorGeneralAdmin)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio', 'get_year_fabricacion', 'p_costo', 'p_venta')
+    list_filter = ('laboratorio', 'f_fabricacion')
+    
+    ordering = ('nombre', 'laboratorio')
+    
+    list_display_link = ('nombre', 'laboratorio')
+
+    list_filter = ('nombre', 'laboratorio')
+
+    def get_year_fabricacion(self, obj):
+        return obj.f_fabricacion.year
+    get_year_fabricacion.short_description = 'F Fabricacion'
+
+admin.site.register (Producto, ProductoAdmin)
+
+
+
+
--- laboratorio/models.py
+++ laboratorio/models.py
@@ -3,11 +3,17 @@ from django.db import models
 # Create your models here.
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length = 128)
+        
+    def __str__(self):
+        return self.nombre
+        
 
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length = 128)
     laboratorio = models.OneToOneField(Laboratorio, on_delete = models.PROTECT)
-
+    
+    def __str__(self):
+        return self.nombre
 
 class Producto(models.Model):
     nombre = models.CharField(max_length = 128)
@@ -15,3 +21,6 @@ class Producto(models.Model):
     f_fabricacion = models.DateField()
     p_costo = models.DecimalField(max_digits = 12, decimal_places = 2)
     p_venta = models.DecimalField(max_digits = 12, decimal_places = 2)
+    
+    def __str__(self):
+        return self.nombre
